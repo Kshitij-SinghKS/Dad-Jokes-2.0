d972b375a57e0ebabb864e403d806e78e580dd75
KshitijSinghKS <kshitijjj717@gmail.com>
2024-04-15 23:24:51 +0530

    File Setup

---
+++ src/App.jsx
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from "react";
+import "./index.css";
+
+import axios from "axios";
+
+function App() {
+  const [joke, setJoke] = useState("");//for jokes api
+  const [buttonColor, setButtonColor] = useState("#3498db");//for random color
+        //getting new joke and assigning random color to randomButton color
+  function handleClick() {
+    const randomButtonColor = getRandomColor();
+    setButtonColor(randomButtonColor);
+    getData();
+  }
+  //generating random color
+  function getRandomColor() {
+    return "#" + Math.floor(Math.random() * 16777215).toString(16);
+  }
+  async function getData() {
+    try {
+      const response = await axios.get("https://icanhazdadjoke.com/", {
+        headers: {
+          Accept: "application/json",
+        },
+      });
+      setJoke(response.data.joke);
+    } catch (error) {
+      console.log(`API Fetch error ${error}`);
+    }
+  }
+
+  //fetch joke only once 
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return (
+    <>
+     <div className="container">
+      <h1 style={{ color: buttonColor }}>Try Not to Laugh 😂</h1>
+      <p className="joke-text">{joke}</p>
+      <div className="button">
+        <button
+          className="btn new-joke-btn"
+          onClick={handleClick}
+          style={{ backgroundColor: buttonColor }}
+        >
+          New Joke
+        </button>
+      </div>
+    </div>
+  );
+    </>
+  )
+}
+
+export default App
